6756b0af5389a302ad102af2154ebf61c357b2b7
as2goo <as2goo@gmail.com>
2018-05-06 22:32:49 +0200

    Added TestConsole

--- OIWrap/Word.cs
+++ OIWrap/Word.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OIWrap;
+using System.Timers;
 
 namespace OIWrap
 {
-    class Word
+    public class Word
     {
         public void Init()
         {
             var oBase = new Main();
+
+            var oApp = new Microsoft.Office.Interop.Word.Application();
+
+            oApp.Visible = true;
+            oApp.Documents.Add();
             /// oBase.StringOut;
         }
     }
---
+++ TestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OIWrap;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oWordApp = new OIWrap.Word();
+
+            Console.Write("Starten von Microsoft Word...");
+            oWordApp.Init();
+            Console.WriteLine("erfolgreich!");
+            Console.ReadKey();
+        }
+    }
+}
